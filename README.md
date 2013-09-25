7fb29d68b7c84133099550f9c63cb3f058ed6a91
Owen Holloway <owen.holloway101@gmail.com>
2013-09-25 20:45:05 +1000

    Got the layout working with the activity

---
+++ README.md
@@ -0,0 +1,5 @@
+Hello, this is a very early stage in development!
+
+=================================================
+
+I hardly even know what I am doing!!
--- src/tk/zeryter/DnDPlaySheetAndroid/DnDPlaySheetAndroidMain.java
+++ src/tk/zeryter/DnDPlaySheetAndroid/DnDPlaySheetAndroidMain.java
@@ -2,14 +2,35 @@ package tk.zeryter.DnDPlaySheetAndroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DnDPlaySheetAndroidMain extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    private Button setName;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main);
+
+        setName = (Button) findViewById(R.id.setName);
+
+
+        setName.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                setName.setText("You clicked me!");
+
+            }
+
+
+        });
+
+
     }
 }
