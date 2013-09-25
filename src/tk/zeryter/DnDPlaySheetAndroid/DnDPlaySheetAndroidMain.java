5c752b0a4bd10409b6c7249f309c0014f3cbd495
Owen Holloway <owen.holloway101@gmail.com>
2013-09-25 19:48:23 +1000

    Files created for "Hello World"

---
+++ src/tk/zeryter/DnDPlaySheetAndroid/DnDPlaySheetAndroidMain.java
@@ -0,0 +1,15 @@
+package tk.zeryter.DnDPlaySheetAndroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DnDPlaySheetAndroidMain extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
