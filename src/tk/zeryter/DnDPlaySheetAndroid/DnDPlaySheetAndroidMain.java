2242b9df79a90ad9c787ebb1d78858f17c70863d
Owen Holloway <owen.holloway101@gmail.com>
2013-09-28 14:30:07 +1000

    Got tabs working!!

--- src/tk/zeryter/DnDPlaySheetAndroid/DnDPlaySheetAndroidMain.java
+++ src/tk/zeryter/DnDPlaySheetAndroid/DnDPlaySheetAndroidMain.java
@@ -1,33 +1,111 @@
 package tk.zeryter.DnDPlaySheetAndroid;
 
 import android.app.*;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import tk.zeryter.DnDPlaySheetAndroid.fragments.BasicDetailsViewFragment;
 import tk.zeryter.DnDPlaySheetAndroid.fragments.RaceFragment;
 
-public class DnDPlaySheetAndroidMain extends Activity {
+public class DnDPlaySheetAndroidMain extends Activity implements ActionBar.TabListener {
 
     //Set the activity for use calling other classes
     private Activity activity = this;
-
     private ActionBar actionBar;
+    private ActionBar.Tab basicDetailsTab, raceTab;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
 
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
+        Log.d("DnDPlayerSheet","Hello World!");
+
         setContentView(R.layout.main);
 
+        //FragmentSetUpAndStuff
+        fragmentManager = getFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        //ActionBar tabs and stuff
         actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        basicDetailsTab = actionBar.newTab();
+        raceTab = actionBar.newTab();
 
-        BasicDetailsViewFragment fragment = new BasicDetailsViewFragment();
+        basicDetailsTab.setTabListener(this);
+        raceTab.setTabListener(this);
 
-        fragmentTransaction.add(R.id.viewSwitcher,fragment);
-        fragmentTransaction.commit();
+        basicDetailsTab.setText("Details");
+        raceTab.setText("Race");
+
+        actionBar.addTab(basicDetailsTab);
+        actionBar.addTab(raceTab);
+
+        fragmentTransaction.add(R.id.viewSwitcher, Variables.basicDetailsFragment);
+        fragmentTransaction.add(R.id.viewSwitcher, Variables.raceFragment);
+//        fragmentTransaction.commit();
 
     }
 
+    public void switchToFragment(Fragment fragment) {
+
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.show(fragment);
+        fragmentTransaction.commit();
+        Log.d("DnDPlayerSheet","fragment show");
+    }
+
+    public void switchFromFragment(Fragment fragment) {
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.hide(fragment);
+        fragmentTransaction.commit();
+        Log.d("DnDPlayerSheet","fragment hide");
+
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        if (tab == basicDetailsTab) {
+            Log.d("DnDPlayerSheet","basicDetailsTab selected");
+            //fragmentTransaction.add(R.id.viewSwitcher,Variables.basicDetailsFragment);
+            switchToFragment(Variables.basicDetailsFragment);
+        }
+
+        if (tab == raceTab) {
+            Log.d("DnDPlayerSheet","raceTab selected");
+            //fragmentTransaction.add(R.id.viewSwitcher,Variables.raceFragment);
+            switchToFragment(Variables.raceFragment);
+        }
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        if (tab == basicDetailsTab) {
+            Log.d("DnDPlayerSheet","basicDetailsTab Unselected");
+            //fragmentTransaction.hide(Variables.basicDetailsFragment);
+            //fragmentTransaction.remove(Variables.basicDetailsFragment);
+            switchFromFragment(Variables.basicDetailsFragment);
+        }
+
+        if (tab == raceTab) {
+            Log.d("DnDPlayerSheet","raceTab Unselected");
+            //fragmentTransaction.hide(Variables.raceFragment);
+            //fragmentTransaction.remove(Variables.raceFragment);
+            switchFromFragment(Variables.raceFragment);
+        }
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
 }
--- src/tk/zeryter/DnDPlaySheetAndroid/Variables.java
+++ src/tk/zeryter/DnDPlaySheetAndroid/Variables.java
@@ -1,5 +1,8 @@
 package tk.zeryter.DnDPlaySheetAndroid;
 
+import android.app.Fragment;
+import tk.zeryter.DnDPlaySheetAndroid.fragments.BasicDetailsViewFragment;
+import tk.zeryter.DnDPlaySheetAndroid.fragments.RaceFragment;
 import tk.zeryter.DnDPlaySheetAndroid.types.Race;
 
 public class Variables {
@@ -9,5 +12,9 @@ public class Variables {
     public static String charName;
     public static int charAge;
     public static Race race;
+
+    //Fragments
+    public static Fragment basicDetailsFragment = new BasicDetailsViewFragment();
+    public static Fragment raceFragment = new RaceFragment();
 
 }
