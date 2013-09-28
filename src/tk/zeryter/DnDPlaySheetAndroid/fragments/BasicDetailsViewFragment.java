3b5806e9dec78c4572fb14d028e1ccdf87029a27
Owen Holloway <owen.holloway101@gmail.com>
2013-09-28 11:44:09 +1000

    Fragments are working and now have methods and those methods do things!

--- src/tk/zeryter/DnDPlaySheetAndroid/fragments/BasicDetailsViewFragment.java
+++ src/tk/zeryter/DnDPlaySheetAndroid/fragments/BasicDetailsViewFragment.java
@@ -1,10 +1,13 @@
 package tk.zeryter.DnDPlaySheetAndroid.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import tk.zeryter.DnDPlaySheetAndroid.R;
 
 public class BasicDetailsViewFragment extends Fragment {
@@ -15,9 +18,35 @@ public class BasicDetailsViewFragment extends Fragment {
         return inflater.inflate(R.layout.basicdetailsfragment, container, false);
     }
 
-    public void onCreate(Bundle savedInstanceState) {
+    TextView setAgePrompt;
 
-        super.onCreate(savedInstanceState);
+    SeekBar setAge;
+
+    public void onStart() {
+
+        super.onStart();
+
+        setAgePrompt = (TextView) getView().findViewById(R.id.setCharAgePrompt);
+
+        setAge = (SeekBar) getView().findViewById(R.id.setCharAge);
+
+
+        setAge.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int currentProgress = seekBar.getProgress();
+                setAgePrompt.setText("Age: " + (currentProgress + 10) );
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
 
     }
 
